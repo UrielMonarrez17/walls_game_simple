afdc5757e3fcca35e4cf59e85155f476b6229229
UrielMonarrez17 <0263097@up.edu.mx>
2026-09-22 18:22:14 -0600

    Small changes

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -14,36 +14,25 @@ public class PlayerMovement : MonoBehaviour
 
     private void Start()
     {
-        // Automatically fetch the Controller script if not assigned manually
+        
         if (controller == null)
             controller = GetComponent<Controller>();
     }
 
     private void Update()
     {
-        // Read Left/Right Arrow or A/D key inputs (-1 to 1)
+      
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
-        // Check for Jump input (Spacebar by default)
         if (Input.GetButtonDown("Jump"))
         {
             jump = true;
         }
 
-        // Check for Crouch input (S or Down Arrow by default)
-        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            crouch = true;
-        }
-        else if (Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.DownArrow))
-        {
-            crouch = false;
-        }
     }
 
     private void FixedUpdate()
     {
-        // Pass the collected inputs to the Controller script
         controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
         jump = false;
     }
